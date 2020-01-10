0809649ce4e29621b64c40659e4b183ab94c4c1e
abbeycheche <abbeyowokoya@yahoo.com>
2020-01-10 13:48:03 -0500

    created a new bash script to install apache

---
+++ my_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+yum install httpd -y
+systemctl start httpd
+echo "Abbey cheche is a very fantastic teacher" >> /var/www/html/index.html
+cat /var/www/html/index.html
+echo "DONE!"
